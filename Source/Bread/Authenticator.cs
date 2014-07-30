bcfd3f0dc76bf1d9a133ee55943e150c0632ec87
ByteBlast <byteblast@gmail.com>
2014-07-30 09:37:42 +0100

    Decoupled authentication code.

---
+++ Source/Bread/Authenticator.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System.Web;
+
+namespace Bread
+{
+    public class Authenticator
+    {
+        private UserManager<IdentityUser> UserManager
+        {
+            get
+            {
+                var store = new UserStore<IdentityUser>();
+                return new UserManager<IdentityUser>(store);
+            }
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get { return HttpContext.Current.GetOwinContext().Authentication; }
+        }
+
+        public IdentityResult Register(string username, string password)
+        {
+            var user = new IdentityUser { UserName = username };
+            var result = UserManager.Create(user, password);
+
+            if (result.Succeeded)
+            {
+                Authenticate(user, false);
+            }
+
+            return result;
+        }
+
+        public bool Login(string username, string password, bool persistant = false)
+        {
+            var user = UserManager.Find(username, password);
+
+            if (user != null)
+            {
+                Authenticate(user, persistant);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Authenticate(IdentityUser user, bool persistent)
+        {
+            var identity = UserManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+            var authenticationProperties = new AuthenticationProperties { IsPersistent = persistent };
+            AuthenticationManager.SignIn(authenticationProperties, identity);
+        }
+
+        public void SignOut()
+        {
+            AuthenticationManager.SignOut();
+        }
+    }
+}
--- Source/Bread/Controllers/AccountController.cs
+++ Source/Bread/Controllers/AccountController.cs
@@ -1,45 +1,28 @@
 ﻿using Bread.Models;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.Owin.Security;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Bread.Controllers
 {
     public class AccountController : Controller
     {
-        private UserManager<IdentityUser> UserManager
-        {
-            get
-            {
-                var store = new UserStore<IdentityUser>();
-                return new UserManager<IdentityUser>(store);
-            }
-        }
-
-        private IAuthenticationManager AuthenticationManager
-        {
-            get { return HttpContext.GetOwinContext().Authentication; }
-        }
+        private readonly Authenticator authenticator = new Authenticator();
 
         public ActionResult Login()
         {
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var user = UserManager.Find(model.Username, model.Password);
-                if (user != null)
+                var authenticated = authenticator.Login(model.Username, model.Password);
+                if (authenticated)
                 {
-                    SignIn(user, model.RememberMe);
                     return RedirectToAction("Index", "Home");
                 }
+
                 ModelState.AddModelError("", "Invalid username or password.");
             }
             return View(model);
@@ -50,19 +33,15 @@ namespace Bread.Controllers
             return View();
         }
 
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Register(RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser {UserName = model.Username};
-                var result = UserManager.Create(user, model.Password);
+                var result = authenticator.Register(model.Username, model.Password);
 
                 if (result.Succeeded)
                 {
-                    SignIn(user, false);
                     return RedirectToAction("Index", "Home");
                 }
 
@@ -74,15 +53,9 @@ namespace Bread.Controllers
             return View(model);
         }
 
-        private void SignIn(IdentityUser user, bool persistent)
-        {
-            var userIdentity = UserManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
-            AuthenticationManager.SignIn(new AuthenticationProperties { IsPersistent = persistent }, userIdentity);
-        }
-
         public ActionResult SignOut()
         {
-            AuthenticationManager.SignOut();
+            authenticator.SignOut();
             return RedirectToAction("Index", "Home");
         }
     }
